1696af37be3f389e7a391dfdf7648e4be085d0db
Nnacyv54 <nancyvsqz8@gmail.com>
2024-07-23 09:45:19 -0500

    Siguientes ejercicios

--- Invertir_unarray.js
+++ Invertir_unarray.js
@@ -2,3 +2,19 @@
 //mismo pero invertido.
 //Ejemplo arreglo original: [23,5,34,56,98,”cadena”,7] el arreglo invertido:
 //[7,”cadena”,98,56,34,5,23].
+
+// para este ejercicio se modifico los valores del ejemplo otorgado
+
+function invertirArray(array) {
+    const invertedArray = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        invertedArray.push(array[i]);
+    }
+    return invertedArray;
+}
+
+const arrayOriginal = [30, 8, 50, 73, 101, "cadena", 9];
+const arrayInvertido = invertirArray(arrayOriginal);
+
+console.log("Array original:", arrayOriginal);
+console.log("Array invertido:", arrayInvertido);
---
+++ Nuevos_jugadores.js
@@ -0,0 +1,104 @@
+/*Clase persona con propiedades nombre, apellido y edad
+1.1. Crea el método getDetalles() el cual imprimirá en consola los atributos de la
+persona.
+1.2. El constructor recibe y asigna nombre, apellido y edad a los atributos internos.
+2. La clase jugador hereda de persona e incluye la propiedad de posición
+2.1. sobreescribir el método getDetalles() para que muestre en consola los detalles
+de persona y los de jugador.
+2.2. El constructor recibe y asigna posición a los atributos internos.
+3. Clase entrenador hereda de persona y agrega años de experiencia y el id de
+federación
+3.1. Sobreescribir el método getDetalles() para que muestre en consola los detalles
+de persona y entrenador.
+_______________________________________________________________________________________________
+_______________________________________________________________________________________________
+3.2. El constructor recibe y asigna posición a los atributos internos.
+3.3. En el constructor configurar para que si no se recibe un id de federación este
+asigne un número aleatorio de 5 dígitos
+4. Clase equipo tiene las propiedades de entrenador y jugadores
+4.1. El constructor recibe el objeto de director y un arreglo con los jugadores que
+pertenecen al equipo (con 3 o 4 jugadores como ejemplo está bien)
+4.2. el método getDetalles() mostrar en consola los detalles del entrenador y de los
+jugadores.
+5. ¡Probemos!
+5.1. crea un arreglo de 4 jugadores con diferentes nombres, edades y posición
+(Portero, Defensa , Medio y Delantero).
+5.2. crea un objeto de tipo entrenador con su nombre, edad y años de experiencia
+5.3. crea un equipo con los objetos de entrenador y estudiantes demostrando el
+funcionamiento de jerarquía.*/
+
+class Persona {
+    constructor (nombre, apellido, edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    getDetalles() {
+        console.log(`Nombre: ${this.nombre} ${this.apellido}`); // Imprime el nombre completo
+        console.log(`Edad: ${this.edad} años`);
+    }
+}
+
+class Jugador extends Persona {
+    constructor (nombre, apellido, edad, posicion) {
+        super(nombre, apellido, edad, posicion);
+        this.posicion = posicion;
+    }
+
+    getDetalles() {
+        super.getDetalles();
+        console.log(`Posición: ${this.posicion}`); 
+    }
+}
+
+class Entrenador extends Persona {
+    constructor(nombre, apellido, edad, aniosExperiencia, idFederacion) {
+      super(nombre, apellido, edad);
+      this.aniosExperiencia = aniosExperiencia;
+      if (idFederacion) {
+        this.idFederacion = idFederacion;
+      } else {
+        this.idFederacion = Math.floor(Math.random() * 10000) + 10000;
+      }
+    }   
+
+    get Detalles() {
+        super.getDetalles();
+        console.log(`Años de experiencia: ${this.aniosExperiencia}`);
+        console.log(`ID de federación: ${this.idFederacion}`);
+    }
+}
+
+class Equipo {
+    constructor ( entrenador, jugadores) {
+        this.entrenador = entrenador;
+        this.jugadores = jugadores;
+    }
+
+    getDetalles(){
+        console.log("**Detalles del Equipo**");
+        console.log("Entrenador:");
+        this.entrenador.getDetalles();
+        console.log("Jugadores:");
+        for (const jugador of this.jugadores){
+            jugador.getDetalles();
+        }
+    }
+}
+
+//Jugadores
+
+const jugador1 = new Jugador("Ana", "Lopez", 18, "Delantero");
+const jugador2 = new Jugador("Maria", "Gomez", 28, "Defensa");
+const jugador3 = new Jugador("Pedro", "Lopez", 30, "Medio");
+const jugador4 = new Jugador("Ana", "Martin", 22, "Delantero");
+
+// Entrenador
+const entrenador = new Entrenador("Carlos", "Sanchez", 45, 10, 12345);
+
+// Equipo
+const equipo = new Equipo(entrenador, [jugador1, jugador2, jugador3, jugador4]);
+
+
+equipo.getDetalles();
